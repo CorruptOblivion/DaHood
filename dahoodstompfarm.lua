8c30e8e2ef6f38b6795021285c16e7e2cad0355c
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-12-18 00:56:48 -0800

    Delete dahoodstompfarm.lua

--- dahoodstompfarm.lua
+++
@@ -1,5 +0,0 @@
-setclipboard("loadstring(game:HttpGet('https://raw.githubusercontent.com/CorruptOblivion/DaHood/main/dahoodbountyfarm.lua'))()")
-game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Oblivion Bounty Farm",
-        Text = "Outdated, copied new loadstring to clipboard.",
-    })
